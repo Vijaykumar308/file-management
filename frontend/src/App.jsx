94aee536d92d2495e88abca593a01e7d11d058e3
vijay kumar <308vijaykumar@gmail.com>
2024-05-21 22:02:24 +0530

    created routes in routes file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,26 +3,23 @@ import './App.css';
 import HomePage from './pages/HomePage';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
+import routes from "./routes/routes";
 
 function App() {
 
   return (
     <>
     <Routes>
-        <Route path="/" element= {<HomePage />} />
-        <Route path="/login" element= {<Login />} />
-        <Route path="/register" element= {<Register />} />
-
-
-      {/* {
-        
-        routes.map((route, index) => {
-          <Route key={index} 
-            path={route.path} 
-            element={route.element} 
-          />
-        })
-      } */}
+       {routes.map((route, index) => {
+          console.log(route.path);
+          return (
+            <Route 
+              key={index} 
+              path={route.path} 
+              element={route.element} 
+            />
+          );
+        })}
     </Routes>
     </>
   )
--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -1,9 +1,11 @@
 import NotFound from "../components/NotFound";
 import HomePage from "../pages/HomePage";
 import Login from "../pages/auth/Login";
+import Register from "../pages/auth/Register";
 
 const routes = [
   { path: '/', element: <HomePage /> },
+  { path: '/register', element: <Register /> },
   { path: '/login', element: <Login /> },
   { path: '*', element: <NotFound /> },
 ];
