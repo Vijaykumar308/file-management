c25f6362747d024a32a7d336f05144b7e523eea9
vijay kumar <308vijaykumar@gmail.com>
2024-05-28 22:57:20 +0530

    trying tocreated protrected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,40 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
 import routes from "./routes/routes";
+import Login from './pages/auth/Login';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const navigator = useNavigate();
+  const [isAuthencated, setIsAuthencated] = useState(true);
+
+  useEffect(()=>{
+    if(!isAuthencated) {
+      navigator('/login');
+    }
+  }, [])
   return (
     <>
     <Routes>
        {routes.map((route, index) => {
-          return (
-            <Route 
-              key={index} 
-              path={route.path} 
-              element={route.element} 
-            />
-          );
-        })}
+        if(route.isPrivate) {
+            return (
+              <Route 
+                key={index} 
+                path={route.path} 
+                element={route.element} 
+              />
+            );
+        }
+
+        return (
+          <Route
+            key={index}
+            path={route.path}
+            element={route.element}
+          />
+        );
+      })}
     </Routes>
     </>
   )
--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -4,10 +4,29 @@ import Login from "../pages/auth/Login";
 import Register from "../pages/auth/Register";
 
 const routes = [
-  { path: '/', element: <HomePage /> },
-  { path: '/register', element: <Register /> },
-  { path: '/login', element: <Login /> },
-  { path: '*', element: <NotFound /> },
+  { 
+    path: '/', 
+    element: <HomePage />,
+    isPrivate:false
+  },
+
+  { 
+    path: '/register', 
+    element: <Register />,
+    isPrivate:true
+  },
+
+  { 
+    path: '/login',
+    element: <Login />,
+    isPrivate:true
+  },
+
+  { 
+    path: '*', 
+    element: <NotFound />,
+    isPrivate:true
+  },
 ];
 
 export default routes;
